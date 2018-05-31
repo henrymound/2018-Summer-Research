5e9e6a0b27e5962f2524a45b477013eecac11a99
Henry Mound <henrymound@gmail.com>
2018-05-31 17:20:36 -0400

    Working Tello Controller w/ Video

--- Tello/camera.go
+++ Tello/camera.go
@@ -4,13 +4,13 @@ package main
      "fmt"
      "os/exec"
      "time"
-
+     "os"
      "gobot.io/x/gobot"
      "gobot.io/x/gobot/platforms/dji/tello"
  )
 
  func main() {
-     drone := tello.NewDriver("8891")
+     drone := tello.NewDriver("0000")
 
      work := func() {
          mplayer := exec.Command("mplayer","-fps", "25", "-")
@@ -29,19 +29,53 @@ package main
              })
          })
 
-         drone.TakeOff()
-         fmt.Println("Take Off")
-
-         gobot.After(5*time.Second, func() {
-             drone.Land()
-         })
-
          drone.On(tello.VideoFrameEvent, func(data interface{}) {
              pkt := data.([]byte)
              if _, err := mplayerIn.Write(pkt); err != nil {
                  fmt.Println(err)
              }
          })
+
+         // After 5 seconds, drone should have opened up a video channel.
+         gobot.After(5*time.Second, func() {
+              fmt.Println("Starting trick loop.")
+              drone.TakeOff()
+              fmt.Println("Take Off")
+              //After 15 seconds in the air
+              gobot.After(10*time.Second, func() {
+                fmt.Println("Front Flip")
+                drone.FrontFlip()
+
+                gobot.After(5*time.Second, func() {
+                  fmt.Println("Back Flip")
+                  drone.BackFlip()
+
+                  gobot.After(5*time.Second, func() {
+                    fmt.Println("Right Flip")
+                    drone.RightFlip()
+
+                    gobot.After(5*time.Second, func() {
+                      fmt.Println("Left Flip")
+                      drone.LeftFlip()
+
+                      gobot.After(5*time.Second, func() {
+
+                        fmt.Println("Land")
+                        drone.Land()
+                        
+                        fmt.Println("Loop complete, terminating program.")
+                        os.Exit(3)
+                      })
+                    })
+                  })
+                })
+              })
+         })
+
+
+
+
+
      }
 
      robot := gobot.NewRobot("tello",
