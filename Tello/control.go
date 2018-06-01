43793c2c5e6596bff64c09d90f197cd8ca5520d4
Henry Mound <henrymound@gmail.com>
2018-06-01 09:43:06 -0400

    Updated program to work for (almost) all commands

--- Tello/control.go
+++ Tello/control.go
@@ -50,23 +50,34 @@ package main
               // Continually ask what flip to do or quit
               for(true){
                 fmt.Print("\nWhat Would You Like to Do?\n")
-                fmt.Print("(m) Move\n(r) Rotate\n(f) Flips\n(q) Quit\n")
+                fmt.Print("(m) Move\n(r) Rotate\n(f) Flips\n(s) Stop Motion\n(q) Quit\n")
                 text, _ := reader.ReadString('\n')
                 text = strings.Replace(text, "\n", "", -1)
 
+                if(text == "s"){
+                  fmt.Print("Stopping Movement...\n")
+                  drone.Forward(0)
+                  drone.Backward(0)
+                  drone.Up(0)
+                  drone.Down(0)
+                  drone.Clockwise(0)
+                  drone.CounterClockwise(0)
+                }
+
                 if(text == "m"){
                   // Get User Input
+                  moveBool := true
                   reader := bufio.NewReader(os.Stdin)
 
                   // Continually ask what flip to do or quit
-                  for(true){
-                    fmt.Print("\nWhere should I go, master?\n")
+                  for(moveBool){
+                    fmt.Print("\nWhere should I go?\n")
                     fmt.Print("(u) Up\n(d) Down\n(f) Forward\n(b) Backward\n(l) Left\n(r) Right\n(q) Quit\n")
                     text, _ := reader.ReadString('\n')
                     text = strings.Replace(text, "\n", "", -1)
 
                     if(text == "u"){
-                      fmt.Print("How fast up should I go, master? (0-100)\n")
+                      fmt.Print("How fast up should I go? (0-100)\n")
                       var i int
                       fmt.Scan(&i)
                       drone.Up(i)
@@ -74,7 +85,7 @@ package main
                     }
 
                     if(text == "d"){
-                      fmt.Print("How fast down should I go, master? (0-100)\n")
+                      fmt.Print("How fast down should I go? (0-100)\n")
                       var i int
                       fmt.Scan(&i)
                       drone.Down(i)
@@ -82,7 +93,7 @@ package main
                     }
 
                     if(text == "f"){
-                      fmt.Print("How fast forward should I go, master? (0-100)\n")
+                      fmt.Print("How fast forward should I go? (0-100)\n")
                       var i int
                       fmt.Scan(&i)
                       drone.Forward(i)
@@ -90,10 +101,26 @@ package main
                     }
 
                     if(text == "b"){
-                      fmt.Print("How fast backward should I go, master? (0-100)\n")
+                      fmt.Print("How fast backward should I go? (0-100)\n")
                       var i int
                       fmt.Scan(&i)
                       drone.Backward(i)
+                      i = 0
+                    }
+
+                    if(text == "l"){
+                      fmt.Print("How fast left should I go? (0-100)\n")
+                      var i int
+                      fmt.Scan(&i)
+                      drone.Left(i)
+                      i = 0
+                    }
+
+                    if(text == "r"){
+                      fmt.Print("How fast right should I go? (0-100)\n")
+                      var i int
+                      fmt.Scan(&i)
+                      drone.Right(i)
                       i = 0
                     }
 
@@ -107,19 +134,60 @@ package main
                     }
 
                     if(text == "q"){
-                      fmt.Println("Land")
-                      drone.Land()
-
-                      fmt.Println("Loop complete, terminating program.")
-                      os.Exit(3)
+                      fmt.Print("Stopping Movement...\n")
+                      drone.Forward(0)
+                      drone.Backward(0)
+                      drone.Up(0)
+                      drone.Down(0)
+                      drone.Clockwise(0)
+                      drone.CounterClockwise(0)
+                      moveBool = false
                     }
 
                   }
                 }
 
                 if(text == "r"){
+                  // Get User Input
+                  rotateBool := true
+                  reader := bufio.NewReader(os.Stdin)
 
+                  // Continually ask what flip to do or quit
+                  for(rotateBool){
+                    fmt.Print("\nHow should I rotate?\n")
+                    fmt.Print("(c) Clockwise\n(cc) Counter Clockwise\n(q) Quit\n")
+                    text, _ := reader.ReadString('\n')
+                    text = strings.Replace(text, "\n", "", -1)
+
+                    if(text == "c"){
+                      fmt.Print("How fast clockwise should I rotate? (0-100)\n")
+                      var i int
+                      fmt.Scan(&i)
+                      drone.Clockwise(i)
+                      i = 0
+                    }
+
+                    if(text == "cc"){
+                      fmt.Print("How fast counterclockwise should I rotate? (0-100)\n")
+                      var i int
+                      fmt.Scan(&i)
+                      drone.CounterClockwise(i)
+                      i = 0
+                    }
+
+                    if(text == "q"){
+                      fmt.Print("Stopping Movement...\n")
+                      drone.Forward(0)
+                      drone.Backward(0)
+                      drone.Up(0)
+                      drone.Down(0)
+                      drone.Clockwise(0)
+                      drone.CounterClockwise(0)
+                      rotateBool = false
+                    }
                 }
+
+              }
 
                 if(text == "f"){
                   // Get User Input
@@ -166,6 +234,14 @@ package main
                 if(text == "q"){
                   fmt.Println("Land")
                   drone.Land()
+
+                  fmt.Println("Flight complete. Terminating program.")
+                  os.Exit(3)
+                }
+
+                if(text == "pq"){
+                  fmt.Println("Palm Landing...")
+                  drone.PalmLand()
 
                   fmt.Println("Flight complete. Terminating program.")
                   os.Exit(3)
