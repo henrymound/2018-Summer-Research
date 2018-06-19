f6324180cb4b5550454d8a0d165b0ddf97372005
Henry Mound <henrymound@gmail.com>
2018-06-19 14:41:59 -0400

    Updated optical_2 and changed ps3 test to try and work with ubuntu

--- references/TelloPy-develop-0.5.0/tellopy/examples/video_effect_dense_optical_2_with_control.py
+++ references/TelloPy-develop-0.5.0/tellopy/examples/video_effect_dense_optical_2_with_control.py
@@ -4,38 +4,150 @@ import tellopy
 import av
 import cv2.cv2 as cv  # for avoidance of pylint error
 import numpy as np
+import pygame
+import pygame.locals
+import time
 #import video
 from common import anorm2, draw_str
 from time import clock
+import threading
+
+
+class JoystickPS3:
+    # d-pad
+    UP = 4  # UP
+    DOWN = 6  # DOWN
+    ROTATE_LEFT = 7  # LEFT
+    ROTATE_RIGHT = 5  # RIGHT
+
+    # bumper triggers
+    TAKEOFF = 11  # R1
+    LAND = 10  # L1
+    # UNUSED = 9 #R2
+    # UNUSED = 8 #L2
+
+    # buttons
+    FORWARD = 12  # TRIANGLE
+    BACKWARD = 14  # CROSS
+    LEFT = 15  # SQUARE
+    RIGHT = 13  # CIRCLE
+
+    # axis
+    LEFT_X = 0
+    LEFT_Y = 1
+    RIGHT_X = 2
+    RIGHT_Y = 3
+    LEFT_X_REVERSE = 1.0
+    LEFT_Y_REVERSE = -1.0
+    RIGHT_X_REVERSE = 1.0
+    RIGHT_Y_REVERSE = -1.0
+    DEADZONE = 0.1
+
+
+class JoystickPS4:
+    # d-pad
+    UP = -1  # UP
+    DOWN = -1  # DOWN
+    ROTATE_LEFT = -1  # LEFT
+    ROTATE_RIGHT = -1  # RIGHT
+
+    # bumper triggers
+    TAKEOFF = 5  # R1
+    LAND = 4  # L1
+    # UNUSED = 7 #R2
+    # UNUSED = 6 #L2
+
+    # buttons
+    FORWARD = 3  # TRIANGLE
+    BACKWARD = 1  # CROSS
+    LEFT = 0  # SQUARE
+    RIGHT = 2  # CIRCLE
+
+    # axis
+    LEFT_X = 0
+    LEFT_Y = 1
+    RIGHT_X = 2
+    RIGHT_Y = 3
+    LEFT_X_REVERSE = 1.0
+    LEFT_Y_REVERSE = -1.0
+    RIGHT_X_REVERSE = 1.0
+    RIGHT_Y_REVERSE = -1.0
+    DEADZONE = 0.08
+
+
+class JoystickXONE:
+    # d-pad
+    UP = 0  # UP
+    DOWN = 1  # DOWN
+    ROTATE_LEFT = 2  # LEFT
+    ROTATE_RIGHT = 3  # RIGHT
+
+    # bumper triggers
+    TAKEOFF = 9  # RB
+    LAND = 8  # LB
+    # UNUSED = 7 #RT
+    # UNUSED = 6 #LT
+
+    # buttons
+    FORWARD = 14  # Y
+    BACKWARD = 11  # A
+    LEFT = 13  # X
+    RIGHT = 12  # B
+
+    # axis
+    LEFT_X = 0
+    LEFT_Y = 1
+    RIGHT_X = 2
+    RIGHT_Y = 3
+    LEFT_X_REVERSE = 1.0
+    LEFT_Y_REVERSE = -1.0
+    RIGHT_X_REVERSE = 1.0
+    RIGHT_Y_REVERSE = -1.0
+    DEADZONE = 0.09
+
+# Global variables
+buttons = JoystickPS3
+speed = 100
+throttle = 0.0
+yaw = 0.0
+pitch = 0.0
+roll = 0.0
+drone = tellopy.Tello()
+
 
 
 def main():
-    drone = tellopy.Tello()
+
 
     lk_params = dict( winSize  = (5, 5),
                       maxLevel = 2,
                       criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
     feature_params = dict( maxCorners = 200,
-                           qualityLevel = 0.0023,
+                           qualityLevel = 0.03,
                            minDistance = 7,
                            blockSize = 7 )
 
     try:
         drone.connect()
-        #drone.wait_for_connection(10.0)
+        drone.wait_for_connection(60.0)
+
 
         track_len = 10
         detect_interval = 5
         tracks = []
         frame_idx = 0
-        VIDEO_SCALE = 0.25
+        VIDEO_SCALE = 0.5
 
         #drone.set_video_encoder_rate(2)
         container = av.open(drone.get_video_stream())
 
+
         while True:
+            time.sleep(0.01)
             for frameRaw in container.decode(video=0):
+
+
                 frame1 = cv.cvtColor(np.array(frameRaw.to_image()), cv.COLOR_RGB2BGR)
                 frame = cv.resize(frame1, (0,0), fx=VIDEO_SCALE, fy=VIDEO_SCALE)
                 frame_gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
@@ -60,7 +172,7 @@ def main():
                         cv.circle(vis, (x, y), 2, (0, 255, 0), -1)
                     tracks = new_tracks
                     cv.polylines(vis, [np.int32(tr) for tr in tracks], False, (0, 255, 0))
-                    #draw_str(vis, (20, 20), 'track count: %d' % len(tracks))
+                    draw_str(vis, (20, 20), 'track count: %d' % len(tracks))
 
                 if frame_idx % detect_interval == 0:
                     mask = np.zeros_like(frame_gray)
