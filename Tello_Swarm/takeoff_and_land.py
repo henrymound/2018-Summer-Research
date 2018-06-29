6808a248d56417527e5a2ba571b925de7d4d41e1
Henry Mound <henrymound@gmail.com>
2018-06-29 12:05:37 -0400

    Added screenshots

--- Tello_Swarm/takeoff_and_land.py
+++ Tello_Swarm/takeoff_and_land.py
@@ -2,16 +2,19 @@ import socket
 import time
 
 sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock1.setsockopt(socket.SOL_SOCKET, 25, 'lo0:'.encode())
+sock1.setsockopt(socket.SOL_SOCKET, 2, 'en0')
 
 sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock2.setsockopt(socket.SOL_SOCKET, 25, 'en8'.encode())
+sock2.setsockopt(socket.SOL_SOCKET,socket.IP_HDRINCL,5)
+
 
 sock1.sendto('command'.encode(), 0, ('192.168.10.1', 8889))
 sock2.sendto('command'.encode(), 0, ('192.168.10.1', 8889))
 
 sock1.sendto('takeoff'.encode(), 0, ('192.168.10.1', 8889))
 sock2.sendto('takeoff'.encode(), 0, ('192.168.10.1', 8889))
+
+time.sleep(5)
 
 sock1.sendto('command'.encode(), 0, ('192.168.10.1', 8889))
 sock2.sendto('command'.encode(), 0, ('192.168.10.1', 8889))
