ab1fb6bdd45ccfaaac44d149f21d340ff4a2aaf2
Henry Mound <henrymound@gmail.com>
2018-07-06 15:36:33 -0400

    Made changes to try and get video from swarm

--- Tello_Swarm/simple_takeoff.py
+++ Tello_Swarm/simple_takeoff.py
@@ -12,8 +12,8 @@ def handler(event, sender, data, **args):
 
 
 def test():
-    drone1 = tellopy.Tello("wlxf8788c004f09")
-    drone2 = tellopy.Tello("wlp1s0")
+    drone1 = tellopy.Tello("wlxf8788c004f09", 9001, 6039)
+    drone2 = tellopy.Tello("wlp1s0", 9002, 6040)
     try:
         drone1.subscribe(drone1.EVENT_FLIGHT_DATA, handler)
         drone2.subscribe(drone2.EVENT_FLIGHT_DATA, handler)
@@ -23,9 +23,6 @@ def test():
         drone2.wait_for_connection(60.0)
         drone1.takeoff()
         drone2.takeoff()
-        sleep(5)
-        drone1.down(50)
-        drone2.down(50)
         sleep(5)
         drone1.land()
         drone2.land()
---
+++ Tello_Swarm/video.py
@@ -0,0 +1,42 @@
+import sys
+import traceback
+import tellopy
+import av
+import cv2.cv2 as cv2  # for avoidance of pylint error
+import numpy
+
+
+def main():
+    drone1 = tellopy.Tello("wlxf8788c004f09", 9000, 6038)
+    drone2 = tellopy.Tello("wlp1s0", 9001, 6039)
+
+    try:
+        drone1.connect()
+        drone2.connect()
+
+        drone1.wait_for_connection(60.0)
+        drone2.wait_for_connection(60.0)
+
+        container1 = av.open(drone1.get_video_stream())
+        container2 = av.open(drone2.get_video_stream())
+        while True:
+            for frame in container1.decode(video=0):
+                image = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+                cv2.imshow('Drone 1', image)
+                cv2.waitKey(1)
+            for frame in container2.decode(video=0):
+                image = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+                cv2.imshow('Drone 2', image)
+                cv2.waitKey(1)
+
+    except Exception as ex:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+        print(ex)
+    finally:
+        drone1.quit()
+        drone2.quit()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
